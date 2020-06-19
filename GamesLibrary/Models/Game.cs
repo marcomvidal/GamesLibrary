efa918f2a3e7ad794c594fa1926df2f02222c817
Marco M. Vidal <marcomvidal@gmail.com>
2020-06-19 14:39:12 -0300

    New game window designed

---
+++ GamesLibrary/Models/Game.cs
@@ -0,0 +1,10 @@
+﻿namespace GamesLibrary.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Cover { get; set; }
+    }
+}
---
+++ GamesLibrary/ViewModels/NewGameViewModel.cs
@@ -0,0 +1,37 @@
+﻿using GamesLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+
+namespace GamesLibrary.ViewModels
+{
+    public class NewGameViewModel : ViewModel
+    {
+        public NewGameViewModel()
+        {
+            Game = new Game
+            {
+                Id = 1,
+                Name = "Sonic the Hedgehog",
+                ReleaseYear = 1991,
+                Cover = Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.MyPictures),
+                    "SonicTheHedgehog.jpg")
+            };
+        }
+
+        private Game _game;
+
+        public Game Game
+        {
+            get => _game;
+            set
+            {
+                _game = value;
+                OnPropertyChanged("Game");
+            }
+        }
+    }
+}
